8f5446f08e3456f74d4d9dc12437cc4788516491
bunnyslayer <142563440+Bunnyslayers@users.noreply.github.com>
2023-09-30 19:18:30 -0700

    Add playerSelection and getComputerChoice functioc
        player selection allows player to select rock, paper, or scissors
    
        get computer choice creates variable between 1 & 3

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -3,3 +3,20 @@
 let rock = 1;
 let paper = 2;
 let scissors = 3;
+
+function getComputerChoice(random) {
+    random = Math.floor( Math.random() * 3) + 1;
+    return random;
+}
+  
+  
+function playerSelection(input) {
+    
+     let selection = input.toLowerCase();
+    return selection;
+
+}
+
+
+const playerChoice = playerSelection() ;
+const computerChoice = getComputerChoice();
