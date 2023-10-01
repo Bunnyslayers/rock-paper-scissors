16a4e6eac345cd6d8dff0162d77b4a4c5310ab1e
bunnyslayer <142563440+Bunnyslayers@users.noreply.github.com>
2023-09-30 20:06:31 -0700

    Comment playerSelection function

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -4,18 +4,19 @@ function getComputerChoice(random) {
     return random;
 }
   
-  
-function playerSelection(input) {
-     let selection = input.toLowerCase();
 
-     if (selection === "rock") {
-        return 1;
-     } else if ( selection === "paper") {
-        return 2;
-     } else ( selection === "scissors") 
-        return 3; 
 
-}
+// function playerSelection(input) {
+//      let selection = input.toLowerCase();
 
-console.log(playerSelection("rock"))
+//      if (selection === "rock") {
+//         return 1;
+//      } else if ( selection === "paper") {
+//         return 2;
+//      } else ( selection === "scissors") 
+//         return 3; 
+
+// }
+
+
 
