18bc41444ed8f11c4f682564d2fe10645b7ac750
bunnyslayer <142563440+Bunnyslayers@users.noreply.github.com>
2023-10-03 12:39:03 -0700

    Add for loop
        forgot to add to git. Lots of trail and error went into this
        commit. If you are reading this send help. Preferably in the form
        of an extra 10 or 50 IQ points

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,13 +1,11 @@
 
-let playerScore = 0;
-let computerScore = 0;
-
-
-const computerChoice = getComputerChoice();
 
 const playerWon = "Hell yeah, you won . . . ";
 const computerWon = "Damn you lost . . . ";
 const playersTied = "Damn yall tied . . . ";
+
+
+
 
 
 function getComputerChoice(random) {
@@ -26,27 +24,36 @@ function getComputerChoice(random) {
 
 function singleGame () {
 
+    var computerChoice = getComputerChoice();
     var playerSelection = prompt("Chose your fighter . . . rock, paper , scissors!").toLowerCase();
+  
+    
 
     if ((playerSelection === "rock") && (computerChoice === "scissors") || 
     (playerSelection === "scissors") && (computerChoice === "paper") || 
     (playerSelection === "paper") && (computerChoice === "rock"))  {
 
-        playerScore += 1;
-        return playerWon + playerSelection + " beat  " + computerChoice + ".";
+        playerScore++;
+        return (playerWon + playerSelection + " beat  " + computerChoice + ".");
     } else if ( (playerSelection === "rock") && (computerChoice === "paper") ||
      (playerSelection === "scissors") && (computerChoice === "rock") ||
      (playerSelection === "paper") && (computerChoice === "scissors"))  {
 
-        computerScore += 1;
-        return computerWon + computerChoice + " beats " + playerSelection  + "."
-     } else  
+        computerScore++;
+        return (computerWon + computerChoice + " beats " + playerSelection  + ".")
+     } else if (playerSelection === computerChoice) {
         return playersTied
-     
-
-
-
+     } else 
+        return "Not a valid choice dummy."
 }
 
 
 
+
+
+for (i = 0; i <= 5; i++) {
+    let playerScore = 0
+    let computerScore = 0
+    console.log(singleGame());
+
+}
