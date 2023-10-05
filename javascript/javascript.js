8351197f1954d7a51d5290b00ceda019f61375e5
bunnyslayer <142563440+Bunnyslayers@users.noreply.github.com>
2023-10-04 18:28:37 -0700

    Add and troubleshoot for loop
        game compleated. added for look - console.logs and variables for
        player and computer score outside of function.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -27,19 +27,21 @@ function singleGame () {
     var computerChoice = getComputerChoice();
     var playerSelection = prompt("Chose your fighter . . . rock, paper , scissors!").toLowerCase();
   
-    
 
     if ((playerSelection === "rock") && (computerChoice === "scissors") || 
     (playerSelection === "scissors") && (computerChoice === "paper") || 
     (playerSelection === "paper") && (computerChoice === "rock"))  {
 
         playerScore++;
+
         return (playerWon + playerSelection + " beat  " + computerChoice + ".");
     } else if ( (playerSelection === "rock") && (computerChoice === "paper") ||
      (playerSelection === "scissors") && (computerChoice === "rock") ||
      (playerSelection === "paper") && (computerChoice === "scissors"))  {
 
         computerScore++;
+
+
         return (computerWon + computerChoice + " beats " + playerSelection  + ".")
      } else if (playerSelection === computerChoice) {
         return playersTied
@@ -49,11 +51,38 @@ function singleGame () {
 
 
 
+var computerScore = 0;
+var playerScore = 0;
 
-
-for (i = 0; i <= 5; i++) {
-    let playerScore = 0
-    let computerScore = 0
-    console.log(singleGame());
-
+for (i = 0; i <= 100; i++) {
+    if ( computerScore === 5){ 
+       console.log("Game over you lost!");
+       break
+       
+    } else if( playerScore === 5) {
+        console.log("Game over you win buddy!")
+        break
+        
+    } else {
+        console.log(singleGame());
+        console.log(computerScore);
+        console.log(playerScore);
+       
 }
+    
+}
+
+
+
+
+// define callable value for win on players end -- else computer gains point
+
+// add 1++ every iteration based on value that outcome of match decided
+
+// assign 1++ to value created to maintain score over all iterations
+
+// stop game when one player recieves 5 points
+
+// clarify difference between LET - VAR - CONST
+
+//
